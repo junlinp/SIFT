815e7d538a918a6754296d7a5bc937c55d2f9e6f
junlinp <junlinp@qq.com>
2020-09-18 19:57:36 +0800

    code comment

--- matcher.h
+++ matcher.h
@@ -298,6 +298,13 @@ void match5(std::vector<Descriptor>& lhs, std::vector<Descriptor>& rhs,
     second_min_distance[i] = 1e300;
     second_min_index[i] = -1;
   }
+  // assume that we have L1 cache with E cachelines.
+  // meanwhile each cacheline can store B points.
+  // in match3 version code.
+  // it seems that we have lhs.size() / B * rhs.size() / B times cache miss.
+  // but if we match the vector in a subset which can be totally store in cache
+  // cache miss can be reduced to lhs.size() / (E / 2 * B) * rhs.size() / b
+  // (we assume each vector in lhs and rhs fill with half of cache)
   for (int b1 = 0; b1 < lhs.size(); b1 += BATCH_SIZE) {
     for (int b2 = 0; b2 < rhs.size(); b2 += BATCH_SIZE) {
       for (int i = b1; i < b1 + BATCH_SIZE; i++) {
@@ -311,6 +318,68 @@ void match5(std::vector<Descriptor>& lhs, std::vector<Descriptor>& rhs,
             __m256 sub = _mm256_sub_ps(v1, v2);
             m_sum = _mm256_fmadd_ps(sub, sub, m_sum);
           }
+          m_sum = _mm256_add_ps(m_sum, _mm256_permute2f128_ps(m_sum, m_sum, 1));
+          m_sum = _mm256_hadd_ps(m_sum, m_sum);
+          float sum = _mm256_cvtss_f32(_mm256_hadd_ps(m_sum, m_sum));
+
+          if (sum < second_min_distance[i]) {
+            second_min_distance[i] = sum;
+            second_min_index[i] = j;
+            if (second_min_distance[i] < min_distance[i]) {
+              std::swap(second_min_distance[i], min_distance[i]);
+              std::swap(second_min_index[i], min_index[i]);
+            }
+          }
+        }
+      }
+    }
+  }
+  for (int i = 0; i < lhs.size(); i++) {
+    // match_idx[i] = min_index[i];
+    if (min_distance[i] < 0.6 * second_min_distance[i]) {
+      match_idx[i] = min_index[i];
+    } else {
+      match_idx[i] = -1;
+    }
+  }
+}
+void match6(std::vector<Descriptor>& lhs, std::vector<Descriptor>& rhs,
+            std::vector<int>& match_idx) {
+  match_idx.resize(lhs.size());
+
+  std::vector<int> min_index(lhs.size());
+  std::vector<double> min_distance(lhs.size());
+  std::vector<int> second_min_index(lhs.size());
+  std::vector<double> second_min_distance(lhs.size());
+
+  const int BATCH_SIZE = 512;
+  for (int i = 0; i < lhs.size(); i++) {
+    min_distance[i] = 1e300;
+    min_index[i] = -1;
+    second_min_distance[i] = 1e300;
+    second_min_index[i] = -1;
+  }
+  for (int b1 = 0; b1 < lhs.size(); b1 += BATCH_SIZE) {
+    for (int b2 = 0; b2 < rhs.size(); b2 += BATCH_SIZE) {
+      for (int i = b1; i < b1 + BATCH_SIZE; i++) {
+        float* p1_ptr = lhs[i].begin();
+        for (int j = b2; j < b2 + BATCH_SIZE; j++) {
+          float* p2_ptr = rhs[j].begin();
+          __m256 m_sum = _mm256_setzero_ps();
+          __m256 m_sum2 = _mm256_setzero_ps();
+          // unroll by hand
+          
+          for (int d = 0; d < 128; d += 16) {
+            __m256 v1 = _mm256_load_ps(p1_ptr + d);
+            __m256 v2 = _mm256_load_ps(p2_ptr + d);
+            __m256 sub = _mm256_sub_ps(v1, v2);
+            m_sum = _mm256_fmadd_ps(sub, sub, m_sum);
+            __m256 v3 = _mm256_load_ps(p1_ptr + d + 8);
+            __m256 v4 = _mm256_load_ps(p2_ptr + d + 8);
+            __m256 sub2 = _mm256_sub_ps(v3, v4); 
+            m_sum2 = _mm256_fmadd_ps(sub2, sub2, m_sum2);
+          }
+          m_sum = _mm256_add_ps(m_sum, m_sum2);
           m_sum = _mm256_add_ps(m_sum, _mm256_permute2f128_ps(m_sum, m_sum, 1));
           m_sum = _mm256_hadd_ps(m_sum, m_sum);
           float sum = _mm256_cvtss_f32(_mm256_hadd_ps(m_sum, m_sum));
--- sift_test.cc
+++ sift_test.cc
@@ -1,101 +1,102 @@
+#include "sift.h"
+
 #include "gtest/gtest.h"
+#include "matcher.h"
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/imgproc.hpp"
-#include "sift.h"
-#include "matcher.h"
 
 class TimeUse {
+ public:
+  TimeUse() { start_ = std::chrono::high_resolution_clock::now(); }
+  ~TimeUse() {
+    std::chrono::milliseconds duration =
+        std::chrono::duration_cast<std::chrono::milliseconds>(
+            std::chrono::high_resolution_clock::now() - start_);
+    std::cout << "Time excaped :" << duration.count() << " milliseconds"
+              << std::endl;
+  }
 
-public:
-    TimeUse() {
-        start_ = std::chrono::high_resolution_clock::now();
-    }
-    ~TimeUse() {
-        std::chrono::milliseconds duration = 
-            std::chrono::duration_cast<std::chrono::milliseconds>  (std::chrono::high_resolution_clock::now() - start_);
-        std::cout << "Time excaped :" << duration.count() << " milliseconds" << std::endl;
-    }
-
-private:
-    std::chrono::steady_clock::time_point start_;
+ private:
+  std::chrono::steady_clock::time_point start_;
 };
 
 TEST(sym, t) {
-    int row = 16;
+  int row = 16;
 
-    for (int i = -row; i < 0; i++) {
-        EXPECT_EQ(-1 - i, symmetrization_functor(i, row));
-    }
+  for (int i = -row; i < 0; i++) {
+    EXPECT_EQ(-1 - i, symmetrization_functor(i, row));
+  }
 
-    for (int i = 0; i < row; i++) {
-        EXPECT_EQ(i, symmetrization_functor(i, row));
-    }
+  for (int i = 0; i < row; i++) {
+    EXPECT_EQ(i, symmetrization_functor(i, row));
+  }
 
-    for (int i = row; i < 2 * row; i++) {
-        EXPECT_EQ(2 * row - 1 - i, symmetrization_functor(i, row));
-    }
+  for (int i = row; i < 2 * row; i++) {
+    EXPECT_EQ(2 * row - 1 - i, symmetrization_functor(i, row));
+  }
 }
 
 TEST(Gaussian, Blur) {
-    std::string file_path = std::string(Test_DIR) + "/a.jpg";
-    cv::Mat img = cv::imread(file_path, cv::IMREAD_GRAYSCALE);
-    cv::imwrite(std::string(Test_DIR) + "/a_gray.jpg", img);
+  std::string file_path = std::string(Test_DIR) + "/a.jpg";
+  cv::Mat img = cv::imread(file_path, cv::IMREAD_GRAYSCALE);
+  cv::imwrite(std::string(Test_DIR) + "/a_gray.jpg", img);
 
-    cv::Mat my_output;
-    GaussianBlur(img, 1.6, my_output);
+  cv::Mat my_output;
+  GaussianBlur(img, 1.6, my_output);
 
-    cv::imwrite(std::string(Test_DIR) + "/a_blur.jpg", my_output);
+  cv::imwrite(std::string(Test_DIR) + "/a_blur.jpg", my_output);
 
-    cv::Mat opencv_output;
-    
-    cv::GaussianBlur(img, opencv_output, cv::Size(0, 0), 1.6, 1.6);
+  cv::Mat opencv_output;
 
-    cv::imwrite(std::string(Test_DIR) + "/a_blur_opencv.jpg", opencv_output);
+  cv::GaussianBlur(img, opencv_output, cv::Size(0, 0), 1.6, 1.6);
 
+  cv::imwrite(std::string(Test_DIR) + "/a_blur_opencv.jpg", opencv_output);
 }
 #include <random>
 TEST(Match, match) {
-    std::vector<Descriptor> lhs;
-    std::vector<int> shuffle;
-    int n = 1024 * 16;
-    std::default_random_engine engine;
-    std::uniform_real_distribution<float> uniform_d(0, 255);
-    for(int i = 0 ; i < n; i++) {
-        Descriptor d;
-        for(int k = 0; k < 128; k++) {
-            d[k] = uniform_d(engine);
-        }
-        lhs.push_back(d);
-        shuffle.push_back(i);
-    };
-    std::random_device rd;
-    std::mt19937 g(rd());
-    std::shuffle(shuffle.begin(), shuffle.end(), engine);
-    std::vector<Descriptor> rhs;
-    for(int i = 0; i < n; i++) {
-        rhs.push_back(lhs[shuffle[i]]);
+  std::vector<Descriptor> lhs;
+  std::vector<int> shuffle;
+  int n = 1024 * 16;
+  std::default_random_engine engine;
+  std::uniform_real_distribution<float> uniform_d(0, 255);
+  for (int i = 0; i < n; i++) {
+    Descriptor d;
+    for (int k = 0; k < 128; k++) {
+      d[k] = uniform_d(engine);
     }
+    lhs.push_back(d);
+    shuffle.push_back(i);
+  };
+  std::random_device rd;
+  std::mt19937 g(rd());
+  std::shuffle(shuffle.begin(), shuffle.end(), engine);
+  std::vector<Descriptor> rhs;
+  for (int i = 0; i < n; i++) {
+    rhs.push_back(lhs[shuffle[i]]);
+  }
 
-    std::vector<int> match_idx;
-    {
-        TimeUse t;
-        match5(rhs, lhs, match_idx);
-        std::cout << "match 5 : " << std::endl;
-    }
-    {
-        TimeUse t;
-        match3(rhs, lhs, match_idx);
-        std::cout << "match 3 : " << std::endl;
-    }
+  std::vector<int> match_idx;
+  {
+    TimeUse t;
+    match6(rhs, lhs, match_idx);
+    std::cout << "match 6 : " << std::endl;
+  }
+  {
+    TimeUse t;
+    match5(rhs, lhs, match_idx);
+    std::cout << "match 5 : " << std::endl;
+  }
+  {
+    TimeUse t;
+    match3(rhs, lhs, match_idx);
+    std::cout << "match 3 : " << std::endl;
+  }
 
-
-
-    for(int i = 0; i < n; i++) {
-        EXPECT_EQ(match_idx[i], shuffle[i]);
-    }
-
+  for (int i = 0; i < n; i++) {
+    EXPECT_EQ(match_idx[i], shuffle[i]);
+  }
 }
 int main() {
-    testing::InitGoogleTest();
-    return RUN_ALL_TESTS();
+  testing::InitGoogleTest();
+  return RUN_ALL_TESTS();
 }
